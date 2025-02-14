a2ff4ee9c121d37845a7cd37eceba6cfd062d432
Cristian-A-Rojas <c.alejandro57175@gmail.com>
2025-02-14 11:15:05 +0100

    Update GOD.sh

--- GOD.sh
+++ GOD.sh
@@ -1,20 +1,19 @@
-Un menú que permita ejecutar las diferentes acciones:
-  Instalación del servicio
-    Con comandos
-
-    Con Ansible
+show_menu() {
+    clear
+    network_info
+    service_status
     
-    Con Docker
-
-  Eliminación del servicio
-
-  Puesta en marcha
-
-  Parada
-
-  Consultar logs:
-    por fecha
-
-    por tipo
-
-  permitir editar las opciones de configuración que te indique el profesor (según el servicio)
+    echo -e "${GREEN}=== Menú Samba ==="
+    echo "1. Instalar Samba"
+    echo "1.1 Instalar Samba con comandos"
+    echo "1.2 Instalar Samba con Ansible"
+    echo "1.3 Instalar Samba con Docker"
+    echo "2. Eliminar Samba"
+    echo "3. Iniciar servicio"
+    echo "4. Detener servicio"
+    echo "5. Consultar logs"
+    echo "6. Editar configuración"
+    echo "7. Reiniciar servicio"
+    echo "8. Salir"
+    echo -e "${NC}"
+}
