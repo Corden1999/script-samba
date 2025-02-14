d2f38913b28112b172acc0508807ec1baa6ef2a2
Corden1999 <cordenteperezmario@gmail.com>
2025-02-14 11:15:30 +0100

    configuracion samba
    en esta actualización hemos modificado el archivo de configuración diciéndole:
    1. donde va a estar el archivo compartido.
    2. hemos puesto un comentario de quien esta haciendo el trabajo
    3. que se pueda escribir
    4.que sea visible para los clientes

--- samba.sh
+++ samba.sh
@@ -11,7 +11,17 @@ sudo apt-get install samba -y
 #Crear una carpeta compartida
 echo "Creando una carpeta compartida..."
 read -p "Donde Quieres crear la carpeta compartida?" ruta
-sudo mkdir -p $ruta
-sudo chmod -R 777 $ruta
+sudo mkdir -p "$ruta"
+sudo chmod -R 777 "$ruta"
 ruta2 = "$ruta/Readme.txt"
 echo "Archivo de prueba" >> $ruta2
+
+#Configurar Samba para compartir la carpeta
+echo "configurando samba..."
+sudo bash -c 'cat >> /etc/samba/smb.conf' <<EOF
+[Carpeta_compartida]
+   comment = samba grupo cristian, pablo y mario
+   path = $ruta
+   browsable = yes
+   read only = no
+EOF
