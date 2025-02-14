cc40de249dabe6abacb5da73f20f6000b5fb05ad
Cristian-A-Rojas <c.alejandro57175@gmail.com>
2025-02-14 11:08:02 +0100

    Create GOD.sh

---
+++ GOD.sh
@@ -0,0 +1,20 @@
+Un menú que permita ejecutar las diferentes acciones:
+  Instalación del servicio
+    Con comandos
+
+    Con Ansible
+    
+    Con Docker
+
+  Eliminación del servicio
+
+  Puesta en marcha
+
+  Parada
+
+  Consultar logs:
+    por fecha
+
+    por tipo
+
+  permitir editar las opciones de configuración que te indique el profesor (según el servicio)
