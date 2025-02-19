8155a03da5b93713e9276d437764ccfe012ad744
Cristian-A-Rojas <c.alejandro57175@gmail.com>
2025-02-19 13:08:04 +0100

    Update GOD.sh

--- GOD.sh
+++ GOD.sh
@@ -1,13 +1,18 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+
 show_menu() {
     clear
     network_info
     service_status
     
     echo -e "${GREEN}=== Menú Samba ==="
+     echo "Seleccione el numero de la accion que quiere realizar"
     echo "1. Instalar Samba"
-    echo "1.1 Instalar Samba con comandos"
-    echo "1.2 Instalar Samba con Ansible"
-    echo "1.3 Instalar Samba con Docker"
     echo "2. Eliminar Samba"
     echo "3. Iniciar servicio"
     echo "4. Detener servicio"
